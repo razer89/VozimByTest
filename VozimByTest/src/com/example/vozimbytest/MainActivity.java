f5b58d978f9b92029d8de647dc0225eba2969982
razer89 <razer0312@gmail.com>
2015-07-11 11:40:16 -0300

    Google play services activated

--- VozimByTest/src/com/example/vozimbytest/MainActivity.java
+++ VozimByTest/src/com/example/vozimbytest/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.vozimbytest;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
